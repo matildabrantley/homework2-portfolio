c5c9d5ad82d07f397b92384d6a794ffb72a9ddae
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-16 16:48:53 -0400

    Put min(1, opacity) limits on opacities, increased default reduction to opacity, then added a black tint filter. This gives the backgrounds a much more consistent look as they move between styles and makes it easier to read foreground text.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,19 @@ const changeOpacity = (e) => {
     const midY = h/2;
 
     //Retrieve distances
-    const reduction = 0.3;
+    const reduction = 0.4;
     const distMouseFromTopLeft = distance(x, y, 0, 0);
     const distMouseFromTopRight = distance(x, y, w, 0);
     const distMouseFromBotLeft = distance(x, y, 0, h);
     const distMouseFromBotRight = distance(x, y, w, h);
     const distFromTopMid = distance(x, y, midX, 0);
     const distFromBotMid = distance(x, y, midX, h);
-    document.getElementById("background1").style.opacity = (h / 3) / (distMouseFromTopLeft+1) - reduction; 
-    document.getElementById("background5").style.opacity = (h / 3) / (distMouseFromTopRight+1) - reduction; 
-    document.getElementById("background3").style.opacity =  (h / 3) / (distMouseFromBotLeft+1) - reduction; 
-    document.getElementById("background6").style.opacity =  (h / 3) / (distMouseFromBotRight+1) - reduction; 
-    document.getElementById("background2").style.opacity =  (h / 3) / (distFromTopMid+1) - reduction - 0.1; 
-    document.getElementById("background4").style.opacity =  (h / 3) / (distFromBotMid+1) - reduction - 0.1; 
+    document.getElementById("background1").style.opacity = Math.min(1, (h / 3) / (distMouseFromTopLeft+1)) - reduction; 
+    document.getElementById("background5").style.opacity = Math.min(1, (h / 3) / (distMouseFromTopRight+1)) - reduction; 
+    document.getElementById("background3").style.opacity = Math.min(1, (h / 3) / (distMouseFromBotLeft+1)) - reduction; 
+    document.getElementById("background6").style.opacity = Math.min(1, (h / 3) / (distMouseFromBotRight+1)) - reduction; 
+    document.getElementById("background2").style.opacity = Math.min(1, (h / 3) / (distFromTopMid+1)) - reduction - 0.1; 
+    document.getElementById("background4").style.opacity = Math.min(1, (h / 3) / (distFromBotMid+1)) - reduction - 0.1; 
 
 }
 
