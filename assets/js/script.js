86a6d41a5e036c2ba49e912d275d20e6231f4c9d
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-15 21:36:02 -0400

    Added distance function. Added two new versions of background image with unique styles.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
 const changeOpacity = (e) => {
-    let x = e.clientX;
-    let y = e.clientY;
+    const x = e.clientX;
+    const y = e.clientY;
+    const w = window.innerWidth;
+    const h = window.innerHeight;
 
-    // document.getElementById("background1").style.opacity = window.innerWidth / (x + window.innerWidth);
-    // document.getElementById("background2").style.opacity = window.innerWidth / (y + window.innerWidth);
-    document.getElementById("background1").style.opacity = x / window.innerWidth;
-    document.getElementById("background2").style.opacity = y / window.innerWidth;
-    // document.getElementById("background1").style.opacity = 0.5 + (x / window.innerWidth)/2;
-    // document.getElementById("background2").style.opacity = 0.5 + (y / window.innerWidth)/2;
+    const reduction = 0.0;
+    document.getElementById("background1").style.opacity = x / w      - reduction;
+    document.getElementById("background2").style.opacity = y / h      - reduction;
+    document.getElementById("background3").style.opacity = w / (x + w) - y / h      - reduction;
+    document.getElementById("background4").style.opacity = (x / w) /2 + (y / h) / 2;
+    //w / (y + w) - x / h - reduction;
+    // document.getElementById("background2").style.opacity = w / (y + w);
+    // document.getElementById("background1").style.opacity = 0.5 + (x / w)/2;
+    // document.getElementById("background2").style.opacity = 0.5 + (y / w)/2;
 }
 // document.onmousemove(changeOpacity(e));
 // document.getElementById("background2").onmousemove = function(event) {changeOpacity(e)}
+
+const distance = (x1, y1, x2, y2) => {
+    const y = x2 - x1;
+    const x = y2 - y1;
+    return Math.sqrt(x * x + y * y);
+}
